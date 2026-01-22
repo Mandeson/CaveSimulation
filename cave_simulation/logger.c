ba5a483d2f9d34f46728da422fe24f4177050a90
Tomasz Klimek <tomkl584@gmail.com>
2026-01-22 20:58:59 +0100

    Split logs into additional program-specific log files

--- cave_simulation/logger.c
+++ cave_simulation/logger.c
@@ -12,6 +12,16 @@
 #include "logger_interface.h"
 #include "operations.h"
 
+int create_log_file(int *fd, const char *filename) {
+    *fd = open(filename, O_CREAT | O_WRONLY, 0600);
+    if (*fd == -1) {
+        perror("logger_init: open");
+        return -1;
+    }
+
+    return 0;
+}
+
 int logger_init(Logger *logger, bool log_to_stdout) {
     logger->log_to_stdout = log_to_stdout;
 
@@ -30,26 +40,37 @@ int logger_init(Logger *logger, bool log_to_stdout) {
     const char *log_directory = "./log";
     char tmp[256];
 
+    // Create the log directory
     if (mkdir(log_directory, 0700) == -1 && errno != EEXIST) {
         perror("logger_init: mkdir 1");
         return -1;
     }
 
+    // Create the program output directory
     snprintf(tmp, 256, "%s/%s", log_directory, time_date);
-    if (mkdir(tmp, 0700) == -1 && errno != EEXIST) {
+    if (mkdir(tmp, 0700) == -1) {
         perror("logger_init: mkdir 2");
         return -1;
     }
 
     snprintf(tmp, 256, "%s/%s/main.txt", log_directory, time_date);
+
     printf("Creating main log file: %s\n", tmp);
     fflush(stdout);
-    int fd = open(tmp, O_CREAT | O_WRONLY, 0600);
-    if (fd == -1) {
-        perror("logger_init: open");
+    if (create_log_file(&logger->file_main, tmp) == -1)
         return -1;
-    }
-    logger->file = fd;
+
+    snprintf(tmp, 256, "%s/%s/TicketClerk.txt", log_directory, time_date);
+    if (create_log_file(&logger->file_ticket_clerk, tmp) == -1)
+        return -1;
+
+    snprintf(tmp, 256, "%s/%s/Guide.txt", log_directory, time_date);
+    if (create_log_file(&logger->file_guide, tmp) == -1)
+        return -1;
+
+    snprintf(tmp, 256, "%s/%s/Visitor.txt", log_directory, time_date);
+    if (create_log_file(&logger->file_visitor, tmp) == -1)
+        return -1;
 
     pthread_create(&logger->logger_thread, NULL, logger_thread_function, logger);
 
@@ -71,9 +92,29 @@ void logger_destroy(Logger *logger) {
     }
     pthread_join(logger->logger_thread, NULL);
 
-    close(logger->file);
+    if (close(logger->file_main) == -1)
+        perror("logger_destroy: close main.txt");
+
+    if (close(logger->file_ticket_clerk) == -1)
+        perror("logger_destroy: close TicketClerk.txt");
+
+    if (close(logger->file_guide) == -1)
+        perror("logger_destroy: close Guide.txt");
+
+    if (close(logger->file_visitor) == -1)
+        perror("logger_destroy: close Visitor.txt");
 
     destroy_message_queue(logger->message_queue);
+}
+
+// Returns true if the message was issued by program 'prog'
+static bool is_log_source(const char *message, const char *prog) {
+    size_t prog_len = strlen(prog);
+    if (strlen(message) < TIMESTAMP_LENGTH + 1 + prog_len)
+        return false;
+    char extracted_prog_name[MAX_PROG_LENGTH] = {0};
+    strncpy(extracted_prog_name, message + TIMESTAMP_LENGTH + 1, prog_len);
+    return strcmp(extracted_prog_name, prog) == 0;
 }
 
 void* logger_thread_function(void *arg) {
@@ -82,21 +123,21 @@ void* logger_thread_function(void *arg) {
     while (true) {
         // Receive log message from message queue
         LogMessage log_message = {0};
-        int res = msgrcv(logger->message_queue, &log_message, sizeof(log_message.mtext), 1, 0);
-        if (res == -1) {
+        int length = msgrcv(logger->message_queue, &log_message, sizeof(log_message.mtext), 1, 0);
+        if (length == -1) {
             if (errno == EINTR)
                 continue;
             perror("logger_thread_function: msgrcv");
             break;
         }
-        if (res == 0) {
+        if (length == 0) {
             fprintf(stderr, "logger_thread_function: empty message\n");
             continue;
         }
 
         char message[sizeof(log_message.mtext) + 1];
-        memcpy(message, log_message.mtext, res);
-        message[res] = 0;
+        memcpy(message, log_message.mtext, length);
+        message[length] = 0;
 
         if (strcmp(message, TERMINATE_TEXT) == 0)
             break;
@@ -106,12 +147,27 @@ void* logger_thread_function(void *arg) {
             puts(message);
         }
 
-        // Add new line character
-        message[res] = '\n';
+        int additional_fd = -1;
+        if (is_log_source(message, "TicketClerk"))
+            additional_fd = logger->file_ticket_clerk;
+        else if (is_log_source(message, "Guide"))
+            additional_fd = logger->file_guide;
+        else if (is_log_source(message, "Visitor"))
+            additional_fd = logger->file_visitor;
 
-        if (write(logger->file, message, res + 1) == -1) {
+        // Add new line character
+        message[length] = '\n';
+
+        if (write(logger->file_main, message, length + 1) == -1) {
             perror("logger_thread_function: write");
             break;
+        }
+
+        if (additional_fd != -1) {
+            if (write(additional_fd, message, length + 1) == -1) {
+                perror("logger_thread_function: write (additional file)");
+                break;
+            }
         }
     }
 
--- cave_simulation/logger.h
+++ cave_simulation/logger.h
@@ -5,11 +5,19 @@
 
 #define TERMINATE_TEXT "terminate"
 
+#define TIMESTAMP_LENGTH 2 + 8
+
+#define MAX_PROG_LENGTH 16
+
 typedef struct {
     bool log_to_stdout;
 
     int message_queue;
-    int file;
+
+    int file_main;
+    int file_ticket_clerk;
+    int file_guide;
+    int file_visitor;
 
     pthread_t logger_thread;
 } Logger;
